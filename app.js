1051708f3cebaf9ec6a0379bf61c63aacfd844bb
Roel <roel@wheelhouse.be>
2019-10-09 13:16:42 +0200

    added check for mailjet

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ import { sendMail } from "./utils/mail";
 import { checkAlerts } from "./repository";
 import mailjet from "node-mailjet";
 
+if (!process.env.RECIPIENTS) {
+    throw new Error("Recipients not defined")
+} else if (!process.env.MAIL_JET_API_KEY || !process.env.MAIL_JET_API_SECRET) {
+    throw new Error("Mailjet not configured")
+}
+
 const mailjetConnection = mailjet.connect(process.env.MAIL_JET_API_KEY, process.env.MAIL_JET_API_SECRET);
 
-if(!process.env.RECIPIENTS) {
-    throw new Error("Recipients not defined")
-}
 checkHealth();
-
 setInterval((checkHealth), process.env.INTERVAL || 300000);
-
-function throwError(alerts) {
-    throw new Error(`Alerts query does not return expected result: \n ${JSON.stringify(alerts, null, 4)}`);
-}
 
 function checkHealth() {
     checkAlerts()
@@ -21,4 +19,8 @@ function checkHealth() {
             ? console.log('HEALTHY!')
             : throwError(alerts))
         .catch(error => sendMail(mailjetConnection, error))
+}
+
+function throwError(alerts) {
+    throw new Error(`Alerts query does not return expected result: \n ${JSON.stringify(alerts, null, 4)}`);
 }
